b76774d06b456fd31cf70079681dfbad9c8805af
christelle wehbe <wehbechristelle@gmail.com>
2015-02-04 20:45:02 +0100

    openerp and init

---
+++ partner_ssnbr/__openerp__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#
+#
+##############################################################################
+
+
+{
+    'name': 'Social Security Number',
+    'version': '1.0',
+    'category': 'Sales',
+    #'complexity': "normal",
+    'description': """
+Register You Social Security Number.
+=========================================================================================================
+
+More information:
+    """,
+    'author': 'Vertel AB',
+    'website': 'http://www.vertel.se',
+    'depends': ['base'],
+    #'init_xml': [],
+    'data': ['partner_ssnbr_view.xml',
+    #'security/ir.model.access.csv',
+   ],
+    
+    'installable': True,
+    #'auto_install': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
